028699a46a7dfc06126fddb2c98b3096316aecf2
omrdeshmukh <65935564+omrdeshmukh@users.noreply.github.com>
2025-07-03 22:46:27 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
 import pandas as pd
+import os
+
+def load_data(datapath='data'):
+    files = ['employees.csv', 'departments.csv', 'roles.csv', 'training.csv',
+             'incidents.csv', 'security_events.csv', 'agentic_ai_log.csv',
+             'blockchain_audit_log.csv', 'policy.csv']
+    dfs = {}
+    for f in files:
+        path = os.path.join(datapath, f)
+        if os.path.exists(path):
+            dfs[f.replace('.csv','')] = pd.read_csv(path)
+        else:
+            dfs[f.replace('.csv','')] = None
+    return dfs
 
 def load_data_from_upload(uploaded_files):
-    """
-    Accepts a list of uploaded Streamlit files, returns a dict of DataFrames keyed by filename (without .csv)
-    """
     dfs = {}
     for file in uploaded_files:
         name = file.name.replace('.csv','')
